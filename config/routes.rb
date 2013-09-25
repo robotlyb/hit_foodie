46e29e0ca67c42b20faecfa807abe903fb5df50e
kgtong <kgtong1992@gmail.com>
2013-09-25 21:05:12 +0800

    重构1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 HitFoodie::Application.routes.draw do
-  get "weixins/show"
 
-  get "weixins/create"
+  #post "/weixin" => "weixin/home#welcome"
 
-  resource :weixin
+  scope :path => "/weixin", :via => :post, :defaults => {:format => 'xml'} do
+    # 欢迎消息
+    root :to => 'weixin/home#welcome', :constraints => Weixin::Router.new(:type => "text", :content => "Hello2BizUser")
+
+    # 回复图文消息
+    root :to => 'weixin/store#show', :constraints => Weixin::Router.new(:type => "text", :content => /^@/)
+
+  end
+
+  # 初次返回验证字符串
+  get "/weixin" => "weixin/home#show"
 end
